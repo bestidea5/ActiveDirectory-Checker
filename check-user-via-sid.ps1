b2ba6674115efde88247296c56f2acc2adfd7777
BestIdea5 <45437092+bestidea5@users.noreply.github.com>
2022-01-10 15:52:52 +0100

    check if source.txt exists added

--- check-user-via-sid.ps1
+++ check-user-via-sid.ps1
@@ -5,24 +5,31 @@ $foname = 'BestIdea5-ad'
 $source = 'source.txt'
 $output = 'output'
 
-$folder = "$folder\$foname" # combine folder and additional variable for specific directory
+$folder = "$folder\$foname" # combine directory and additional variable for specific directory
 
+clear
 
 if (Test-Path -d -Path $folder) {
-    $users = Get-Content -Path "$folder\$source" -TotalCount 20
-    foreach ($user in $users) {
-        $userobj = $(try {Get-ADUser $user} catch {$Null})
-        If ($userobj -ne $Null) {
-            If ($userobj.enabled -eq "True")
-                {
-                    Write-Output "$user" | Out-File "$folder\$output-users-enabled.txt" -Append #User enabled!
+    if (Test-Path "$folder\$source" -PathType leaf) {
+        $users = Get-Content -Path "$folder\$source" #-TotalCount 20        # Testtry with first 20 lines?
+        foreach ($user in $users) {
+            $userobj = $(try {Get-ADUser $user} catch {$Null})
+            If ($userobj -ne $Null) {
+                If ($userobj.enabled -eq "True") {
+                        Write-Output "$user" | Out-File "$folder\$output-users-enabled.txt" -Append #User enabled!
                 } else {
-            Write-Output "$user" | Out-File "$folder\$output-users-disabled.txt" -Append #User disabled
+                Write-Output "$user" | Out-File "$folder\$output-users-disabled.txt" -Append #User disabled
+                }
+            } else {
+            Write-Output "$user" | Out-File "$folder\$output-users-deleted.txt" -Append #User not available
             }
-        } else {
-    Write-Output "$user" | Out-File "$folder\$output-users-deleted.txt" -Append #User not available
-    }}
-    
+        }
+    } else {
+        Write-Host "File ($source) does not in directory ($folder\) exist!" -ForegroundColor Red
+        Write-Host "Please create this file and add one SID per line" -ForegroundColor Gray
+        Break
+    }
+    Write-Host "Done!" -ForegroundColor Green
 } else {
     New-Item $folder -ItemType directory # create specific directory
 }
